2df9a068f3ca87d6b416dfb5dd2aa8f53a149dd5
KamillaWilliam29 <51264700+KamillaWilliam29@users.noreply.github.com>
2022-05-17 16:31:00 -0300

    Aula dia 10 de maio de 2022
    Aprendemos o que DOM em Javascript. E exercitamos um pouco o uso de certas propriedades em aula!

---
+++ TesteSoma.js
@@ -0,0 +1,11 @@
+//var number = number.parseFloat(window.prompt("Digite um número"))
+
+//Digita outro número
+
+//Some os doi números e exiba o resultado no alert
+
+var n1 = window.prompt("Digite um número: ")
+var n2 = window.prompt("Digite outro número: ")
+var soma = parseInt(n1) + parseInt(n2)
+parseInt(soma)
+window.alert(`A soma dos números digitados é : ${soma}`)
