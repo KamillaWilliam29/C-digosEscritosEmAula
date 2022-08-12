b3c03228bf9cb952646d98d6d7a9bc7119e0513a
KamillaWilliam29 <51264700+KamillaWilliam29@users.noreply.github.com>
2022-08-11 21:07:50 -0300

    ProjetoAulaSpring
    Códigos escritos usando o SpringBoot.

---
+++ ProjetoAulaSpring/src/main/java/br/com/senac/ProjetoAulaSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.senac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoAulaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoAulaSpringApplication.class, args);
+	}
+
+}
