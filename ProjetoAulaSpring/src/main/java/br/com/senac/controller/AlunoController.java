4d283c553f6ff3aca8b415e6ff1b0e6cf6c5eb5d
KamillaWilliam29 <51264700+KamillaWilliam29@users.noreply.github.com>
2022-09-16 20:08:02 -0300

    Controllers atualizados
    Contribuição do aluno Felipe Bonfim

--- ProjetoAulaSpring/src/main/java/br/com/senac/controller/AlunoController.java
+++ ProjetoAulaSpring/src/main/java/br/com/senac/controller/AlunoController.java
@@ -3,22 +3,37 @@ package br.com.senac.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.com.senac.entity.Aluno;
 import br.com.senac.service.AlunoService;
 
 @Controller
 @RequestMapping("aluno")
 public class AlunoController {
-
 	@Autowired
-	private AlunoService service;
+	private AlunoService alunoService;
 	
-	@GetMapping("listarAlunos")
-	public ModelAndView listarTodosAlunos() {
-		ModelAndView mv = new ModelAndView("aluno/paginaListaAluno");
-		mv.addObject("alunos",service.buscarTodosAlunos());
+	@GetMapping("/listarAlunos")
+	public ModelAndView listaTodosAlunos() {
+		ModelAndView mv = new ModelAndView("aluno/paginaListaAlunos");
+		mv.addObject("alunos", alunoService.selectAll());
 		return mv;
 	}
+	
+	@GetMapping("/cadastrarAluno")
+	public ModelAndView cadastrarAluno() {
+		ModelAndView mv = new ModelAndView("aluno/cadastrarAluno");
+		 mv.addObject("aluno", new Aluno());
+		 return mv;
+	}
+	
+	@PostMapping("/salvar")
+	public ModelAndView salvarAluno(Aluno aluno) {
+		alunoService.insert(aluno);
+		return listaTodosAlunos();
+	}
+	//http://localhost:8080/aluno/cadastrarAluno
 }
--- ProjetoAulaSpring/src/main/java/br/com/senac/controller/TurmaController.java
+++ ProjetoAulaSpring/src/main/java/br/com/senac/controller/TurmaController.java
@@ -3,23 +3,37 @@ package br.com.senac.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.com.senac.entity.Turma;
 import br.com.senac.service.TurmaService;
 
 @Controller
 @RequestMapping("turma")
 public class TurmaController {
-
-
 	@Autowired
-	private TurmaService service;
+	private TurmaService turmaService;
 	
-	@GetMapping("listarTurmas")
-	public ModelAndView listarTodosTurmas() {
-		ModelAndView mv = new ModelAndView("turma/paginaListaTurma");
-		mv.addObject("turmas",service.buscarTodosTurmas());
+	@GetMapping("/listarTurmas")
+	public ModelAndView listaTodasTurmas() {
+		ModelAndView mv = new ModelAndView("turma/paginaListaTurmas");
+		mv.addObject("turma", turmaService.selectAll());
 		return mv;
 	}
+
+	@GetMapping("/cadastrarTurma")
+	public ModelAndView cadastrarCurso() {
+		ModelAndView mv = new ModelAndView("turma/cadastrarTurma");
+		 mv.addObject("turma", new Turma());
+		 return mv;
+	}
+	
+	@PostMapping("/salvar")
+	public ModelAndView salvarCurso(Turma turma) {
+		turmaService.insert(turma);
+		return listaTodasTurmas();
+	}
+
 }
