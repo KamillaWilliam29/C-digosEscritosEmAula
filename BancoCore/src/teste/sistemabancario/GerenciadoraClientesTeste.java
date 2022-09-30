25d13460c4457b328f7c2a3a6f605dfc3b0e7619
KamillaWilliam29 <51264700+KamillaWilliam29@users.noreply.github.com>
2022-09-30 20:36:40 -0300

    Criamos a classe de Tete para Gerenciadora Contas e Alterar Gerenciadora Clientes Teste
    30/09

--- BancoCore/src/teste/sistemabancario/GerenciadoraClientesTeste.java
+++ BancoCore/src/teste/sistemabancario/GerenciadoraClientesTeste.java
@@ -1,16 +1,14 @@
 package sistemabancario;
 
 import static org.hamcrest.CoreMatchers.*;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThat;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Test;
-import sistemabancario.GerenciadoraClientes;
 
-public class GerenciadoraClientesTeste {
+public class GerenciadoraClienteTeste {
 	@Test
 	public void testePesquisaCliente() {
 		/*criando clientes para instaciar no caso de Teste*/
@@ -18,78 +16,16 @@ public class GerenciadoraClientesTeste {
 		Cliente cliente01 = new Cliente(1, "Clayton", 47, "clayton@gmail.com", 1 ,true);
 		Cliente cliente02 = new Cliente(2, "Maria", 10, "maria@gmail.com", 1 ,true);
 		
-		List <Cliente> clientes = new ArrayList<>();
+		List <Cliente> clientes = new ArrayList();
 		clientes.add(cliente01);
 		clientes.add(cliente02);
 		
 		GerenciadoraClientes gerClientes = new GerenciadoraClientes(clientes);
 	
-		/*até esta linha nós só criamos e preparamos o nosso ambiente de Teste.*/
 		Cliente cliente = gerClientes.pesquisaCliente(1);
 		
-		/*O Assert that dá uma expectativa de item que retornará ao teste que você deseja realizar */
 		assertThat(cliente.getId(), is(1));
 		assertThat(cliente.getEmail(), is("clayton@gmail.com"));
-		assertThat(cliente.getNome(), is("Clayton"));
-		
-		Cliente cliente2 = gerClientes.pesquisaCliente(2);
-		
-		assertThat(cliente2.getId(), is(2));
-		assertThat(cliente2.getEmail(), is("maria@gmail.com"));
-		assertThat(cliente2.getNome(), is("Maria"));
-		
-	}
-	/*esse método não deve rtornar nenhum objeto. deve ser um void */
-	@Test 
-	public void testeAdicionaCliente() {
-		
-		List <Cliente> clientes = new ArrayList<>();
-		GerenciadoraClientes gerClientes = new GerenciadoraClientes(clientes);
-		Cliente cliente01 = new Cliente(1, "Clayton", 47, "clayton@gmail.com", 1 ,true);
-		gerClientes.adicionaCliente(cliente01);
-		
-		Cliente cliente02 = new Cliente(2, "José Roberto", 30, "parente.25@gmail.com", 1, true);
-		gerClientes.adicionaCliente(cliente02);
-		
-	}
-	/*Criar um google docs para os Caso de Teste e um google docs com o Plano de Teste
-	 * (este documento mdede a cobertura dos seus testes como um todo)*/
-	
-
-	@Test
-	public void testeRemoveCliente() {
-		List <Cliente> clientes = new ArrayList<>();
-		GerenciadoraClientes gerClientes = new GerenciadoraClientes(clientes);
-		Cliente cliente03 = new Cliente(3, "João Simões", 21, "joaos@gmail.com", 3 , true);
-		Cliente cliente5 = new Cliente (5, "Ana Carolina", 26, "carolana@gmail.com", 5, true);
-		gerClientes.adicionaCliente(cliente03);
-		gerClientes.pesquisaCliente(3);
-		
-		//execução do teste
-		boolean clienteRemovido = gerClientes.removeCliente(3);
-		boolean clienteRemovido2 = gerClientes.removeCliente(4);
-		gerClientes.adicionaCliente(cliente5);
-		
-		//gerClientes.removeCliente(3);
-		
-		//análise da remoção esperada.
-		assertThat(clienteRemovido, is(true));
-		assertThat(gerClientes.getClientesDoBanco().size(), is(0));
-		assertNull(gerClientes.pesquisaCliente(3));
-		assertThat(clienteRemovido2, is(false));
-		//teste do método remover. Para assegurar que removemos com sucesso um cliente, adicionamos e pesquisamos ele só em caso, pois o método remoção precisa de clientes adicionados
-	}
-	
-	@Test
-	public void testeIsAtivo() {
-		List <Cliente> clientes = new ArrayList<>();
-		GerenciadoraClientes gerClientes = new GerenciadoraClientes(clientes);
-		Cliente cliente04 = new Cliente(4, "Caio", 18, "caio@gmail.com", 4, true);
-		Cliente cliente05 = new Cliente(5, "Ricardo Carvalho", 62, "ricardo@gmail.com", 5, false);
-		gerClientes.clienteAtivo(4);
-		gerClientes.clienteAtivo(5);
-		
-		//o método cliente Ativo testa se um determinado clietne está ativo ou não. Buscammos o cliente por ID, verificamos se o metodo clienteAtivo é igual a true, e retornamos o cliente ativo. Para prepararmos o ambiente de teste, adicionamos dois clientes: um com valor booleano true e outro com false.
 	}
 	
 }
