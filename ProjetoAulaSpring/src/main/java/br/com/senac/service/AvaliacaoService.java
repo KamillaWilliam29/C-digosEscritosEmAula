11c618662d8300068bdf7a2eca8baa831394c7c6
KamillaWilliam29 <51264700+KamillaWilliam29@users.noreply.github.com>
2022-10-20 21:54:31 -0300

    Add files via upload

---
+++ ProjetoAulaSpring/src/main/java/br/com/senac/service/AvaliacaoService.java
@@ -0,0 +1,25 @@
+package br.com.senac.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.senac.entity.Avaliacao;
+import br.com.senac.repository.AvaliacaoRepository;
+
+@Service
+public class AvaliacaoService {
+
+		@Autowired
+		AvaliacaoRepository repo;
+		
+		public Avaliacao save (Avaliacao avaliacao) {
+			return repo.save(avaliacao);
+			
+		}
+		
+		public List <Avaliacao> findAll(){
+			return repo.findAll();
+		}
+}
