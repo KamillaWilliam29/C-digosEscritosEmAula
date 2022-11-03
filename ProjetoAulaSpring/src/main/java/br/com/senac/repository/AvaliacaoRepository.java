97f3adac76d26f63c2a9dab04813cdae9ba91ea2
KamillaWilliam29 <51264700+KamillaWilliam29@users.noreply.github.com>
2022-11-03 18:57:18 -0300

    Add files via upload

--- ProjetoAulaSpring/src/main/java/br/com/senac/repository/AvaliacaoRepository.java
+++ ProjetoAulaSpring/src/main/java/br/com/senac/repository/AvaliacaoRepository.java
@@ -4,9 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.senac.entity.AlunoCurso;
-import  br.com.senac.entity.Avaliacao;
+import br.com.senac.entity.Avaliacao;
 
 @Repository
-public interface AvaliacaoRepository extends JpaRepository <Avaliacao, AlunoCurso> {
-
+public interface AvaliacaoRepository extends JpaRepository<Avaliacao, AlunoCurso> {
+    
 }
