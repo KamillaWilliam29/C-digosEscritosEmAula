b3459a9ee78ab6d9fd7295a6842fe448a09d83f2
KamillaWilliam29 <51264700+KamillaWilliam29@users.noreply.github.com>
2022-11-04 18:15:31 -0300

    EndereçoService Criada
    E por criamos a classe com as regras de Negocio de Endereço.

---
+++ ProjetoAulaSpring/src/main/java/br/com/senac/service/EndereçoService.java
@@ -0,0 +1,48 @@
+package br.com.senac.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.senac.entity.Endereço;
+import br.com.senac.repository.EndereçoRepository;
+
+@Service
+public class  EndereçoService {
+	@Autowired
+	EndereçoRepository repo;
+	public List<Endereço> selectAll(){
+		return repo.findAll();
+	}
+	
+	public Endereço cadastrarEndereço(Endereço endereço) {
+		return repo.save(endereço);
+	}
+	
+	public Endereço buscarEndereço(Integer id) {
+		return repo.findById(id).get();
+	}
+	
+	public void excluirEndereço(Integer id) {
+		repo.deleteById(id);
+	}
+	
+	public void update(Endereço endereçoAlterado) {
+		Endereço endereço = buscarEndereço(endereçoAlterado.getId());
+		endereço.setRua(endereçoAlterado.getRua());
+		endereço.setNumero(endereçoAlterado.getNumero());
+		endereço.setComplemento(endereçoAlterado.getComplemento());
+		endereço.setBairro(endereçoAlterado.getBairro());
+		cadastrarEndereço(endereço);
+	}
+	public Endereço salvarAlteraçao(Endereço endereçoAlterado) {
+		Endereço endereço = buscarEndereço(endereçoAlterado.getId());
+		endereço.setRua(endereçoAlterado.getRua());
+		endereço.setNumero(endereçoAlterado.getNumero());
+		endereço.setComplemento(endereçoAlterado.getComplemento());
+		endereço.setBairro(endereçoAlterado.getBairro());
+		return repo.save(endereço);
+	}
+	
+}
